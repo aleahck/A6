b320634d4f3b7c79f4c084c966d8b6bcaccb07c5
aleahck <ack237@cornell.edu>
2015-11-06 16:17:49 -0500

    updated gamestate.mli to have types for game and player

--- gamestate.mli
+++ gamestate.mli
@@ -4,4 +4,22 @@ open Logic
 
 (*GAMESTATE MLI*)
 
-(**)
+(*Type player contains user data*)
+type player ={
+    stake: int;
+    cards: card * card;
+    best: hand (*whats this being called from logic?*)
+  }
+
+
+(*Type game contains info on bets, deck*)
+type game= {
+    flop: card list;
+    bet: int;
+    pot: int;
+    players: player list
+    deck: deck
+  }
+    
+
+
