0efe7c491a710bf658cc105822285bcec3e2f669
ncd29 <ncd29@cornell.edu>
2015-12-02 15:40:26 -0500

    compare_hands

--- gamelogic.ml
+++ gamelogic.ml
@@ -30,17 +30,16 @@ let rec insertion_sort (l:card list) : card list =
               else h1::insert elem t1
 
 (* insertion sort for sorting by a cards SUIT useful for sorting lists of cards
-* Suit order is Spades > Clubs > Diamonds > Hearts *
-* TEST THIS *)
+* Suit order is Spades > Clubs > Diamonds > Hearts *)
 let rec insertion_sort_suit (l:card list) : card list =
   match l with
   | [] -> []
-  | h::t -> insert h (insertion_sort t)
-  and insert elem lst =
+  | h::t -> insert2 h (insertion_sort_suit t)
+  and insert2 elem lst =
   match lst with
   | [] -> [elem]
-  | h1::t1 -> if (suit_of_card elem) < (suit_of_card h1) then elem::h1::t1
-            else h1::insert elem t1
+  | h1::t1 -> if (suit_of_card elem) > (suit_of_card h1) then elem::h1::t1
+            else h1::insert2 elem t1
 
 (* converts a hand to a card list *)
 let hand_to_card_list (h:hand) : card list =
@@ -57,12 +56,12 @@ let hand_to_card_list (h:hand) : card list =
   | RoyalFlush clist -> clist
 
 (* returns the suit that occurs most frequently in the card list
-* and the number of times it occurs as a pair
+* and the number of times it occurs as a suit * int pair
 * if more than one suit occurs the same amount of times, the
-return does not matter, since this function is only used for
-finding flushes, and since we are looking for flushes in a
-maximum of 7 cards, if more than one suit does occur the same
-amount of times, this means there cannot possibly be a flush *)
+* return does not matter, since this function is only used for
+* finding flushes, and since we are looking for flushes in a
+* maximum of 7 cards, if more than one suit does occur the same
+* amount of times, this means there cannot possibly be a flush *)
 let most_common_suit (c:card list) : suit * int =
 let rec loop clist h d cl s =
   match clist with
@@ -133,6 +132,7 @@ let flush_check (c:card list) : bool =
   in loop c 0
 
 (*check if a card list contains a straight *)
+(*Does not work *)
 let straight_check (c:card list) : bool =
   let rec loop clist acc =
     match (insertion_sort clist) with
@@ -145,25 +145,33 @@ let straight_check (c:card list) : bool =
     | _ -> acc >= 5
   in loop c 0
 
-(*check if a card list contains three of a kind*)
+(*check if a card list contains three of a kind
+* if the triple is part of four of a kind it will return false*)
 let rec triple_check (c:card list) : bool =
-  match (insertion_sort c) with
-  | h1::h2::h3::t -> if val_of_card h1 = val_of_card h2 &&
-                     val_of_card h2 = val_of_card h3 &&
-                     not (List.mem (val_of_card h1) (List.map val_of_card c))
-                     then true
-                     else triple_check (h2::h3::t)
-  | _ -> false
+  let rec loop clist acc =
+    match (insertion_sort clist) with
+    | h1::h2::h3::t -> if val_of_card h1 = val_of_card h2 &&
+                       val_of_card h2 = val_of_card h3 &&
+                       not (List.mem (val_of_card h1) (List.map val_of_card t)) &&
+                       not (List.mem (val_of_card h1) (List.map val_of_card acc))
+                       then true
+                       else loop (h2::h3::t) (acc@[h1])
+    | _ -> false
+  in loop c []
 
 (*returns the first pair in the card list - this is fine
-* because determine best hand will have already checked for everything else*)
-let rec pair_check (c:card list) : bool =
-  match (insertion_sort c) with
-  | h1::h2::t -> if val_of_card h1 = val_of_card h2 &&
-                 not (List.mem (val_of_card h1) (List.map val_of_card c))
-                 then true
-                 else pair_check (h2::t)
-  | _ -> false
+* because determine best hand will have already checked for everything else
+* if the pair is part of three or four of a kind it wil return false*)
+let pair_check (c:card list) : bool =
+  let rec loop clist acc =
+    match (insertion_sort clist) with
+    | h1::h2::t -> if val_of_card h1 = val_of_card h2 &&
+                   not (List.mem (val_of_card h1) (List.map val_of_card t)) &&
+                   not (List.mem (val_of_card h1) (List.map val_of_card acc))
+                   then true
+                   else loop (h2::t) (acc@[h1])
+    | _ -> false
+  in loop c []
 
 (*check if a card list contains a full house*)
 let rec full_house_check (c:card list) : bool =
@@ -180,19 +188,21 @@ let two_pair_check (c:card list) : bool =
                    else pair_check (h2::t)
   in loop c 0
 
-
 (*helpers for compare_hands
 *invariant: all card lists have 5 cards upon initial function call?*)
 
-(*HighCard comparison*)
-let rec compare_high_card (h1:card list) (h2:card list) : card list =
-  match (insertion_sort h1, insertion_sort h2) with
-  | ([],[]) -> failwith "same hands" (*both hands are the same?*)
-  | (x::xs,[]) -> h1 (*code should never actually reach here*)
-  | ([],x::xs) -> h2 (*code should never actually reach here*)
-  | (x1::xs1,x2::xs2) -> if (val_of_card x1) > (val_of_card x2) then h1
-                         else if (val_of_card x1) < (val_of_card x2) then h2
-                         else compare_high_card xs1 xs2
+(*HighCard comparison - return whichever hand (card list) is better*)
+let compare_high_card (h1:card list) (h2:card list) : card list =
+  let rec loop hand1 hand2 =
+    match (hand1,hand2) with
+    | ([],[]) -> let r = Random.int 2 in
+                 if r = 0 then h1 else h2
+    | (x::xs,[]) -> h1 (*code should never reach here - same for other compares*)
+    | ([],x::xs) -> h2 (*code should never reach here - same for other compares*)
+    | (x1::xs1,x2::xs2) -> if (val_of_card x1) > (val_of_card x2) then h1
+                           else if (val_of_card x1) < (val_of_card x2) then h2
+                           else loop xs1 xs2
+                         in loop (insertion_sort h1) (insertion_sort h2)
 
 (* returns the card value of the pair in the hand that contains a pair
 * card list h is guaranteed to contain at least a pair *)
@@ -202,7 +212,7 @@ let rec value_of_pair (h:card list) : value =
                  else value_of_pair (h2::t)
   | _ -> failwith "violated precondition"
 
-(*Pair comparison - returns which hand is better*)
+(*Pair comparison - returns the hand that is better*)
 let compare_pair (h1:card list) (h2:card list) : card list =
   let v1 = value_of_pair (insertion_sort h1) in
     let v2 = value_of_pair (insertion_sort h2) in
@@ -210,16 +220,16 @@ let compare_pair (h1:card list) (h2:card list) : card list =
       else if v1 < v2 then h2
       else let rec loop hnd1 hnd2 =
         match (insertion_sort hnd1, insertion_sort hnd2) with
-        | ([],[]) -> failwith "same hands" (*both hands are the same?*)
+        | ([],[]) -> let r = Random.int 2 in
+                     if r = 0 then h1 else h2
         | (x::xs,[]) -> h1
         | ([],x::xs) -> h2
         | (x1::xs1,x2::xs2) -> if v1 = val_of_card x1 then loop xs1 xs2
-                          else if (val_of_card x1) < (val_of_card x2) then hnd2
-                          else hnd1
+                          else if (val_of_card x1) < (val_of_card x2) then h2
+                          else h1
                         in loop h1 h2
 
-(* TwoPair comparison *)
-
+(* TwoPair comparison - returns the hand that is better*)
 let compare_two_pair (h1:card list) (h2:card list) : card list =
   let largerPair1 = val_of_card (List.nth (insertion_sort h1) 1) in
     let largerPair2 = val_of_card (List.nth (insertion_sort h2) 1) in
@@ -230,17 +240,19 @@ let compare_two_pair (h1:card list) (h2:card list) : card list =
       if smallerPair1 > smallerPair2 then h1
       else if smallerPair1 < smallerPair2 then h2
       else let rec loop hnd1 hnd2 =
-           match (insertion_sort hnd1, insertion_sort h2) with
-           | ([],[]) -> failwith "same hands" (*both hands are the same?*)
+           match (insertion_sort hnd1, insertion_sort hnd2) with
+           | ([],[]) -> let r = Random.int 2 in
+                        if r = 0 then h1 else h2
            | (x::xs,[]) -> h1
            | ([],x::xs) -> h2
            | (x1::xs1,x2::xs2) ->
               if (val_of_card x1 = val_of_card x2) then loop xs1 xs2
-              else compare_high_card [x1] [x2]
+              else if compare_high_card [x1] [x2] = [x1] then h1 else h2
             in loop h1 h2
 
-(* Triple comparison *
-* card list h must be known to guarantee at least 3 cards with the same value*)
+
+(* card list h must be known to guarantee at least 3 cards with the same value
+* returns the card value of the cards that make up the triple*)
 let rec value_of_triple (h:card list) : value =
   match h with
   | h1::h2::h3::t -> if (same_value h1 h2) && (same_value h2 h3)
@@ -248,6 +260,7 @@ let rec value_of_triple (h:card list) : value =
                      else value_of_triple (h2::h3::t)
   | _ -> failwith "violated precondition"
 
+(* Triple comparison - returns the better hand *)
 let compare_triple (h1:card list) (h2:card list) : card list =
   let v1 = value_of_triple (insertion_sort h1) in
     let v2 = value_of_triple (insertion_sort h2) in
@@ -255,81 +268,100 @@ let compare_triple (h1:card list) (h2:card list) : card list =
       else if v1 < v2 then h2
       else let rec loop hnd1 hnd2 =
         match (insertion_sort hnd1, insertion_sort hnd2) with
-        | ([],[]) -> failwith "same hands" (*both hands are the same?*)
+        | ([],[]) -> let r = Random.int 2 in
+                     if r = 0 then h1 else h2
         | (x::xs,[]) -> h1
         | ([],x::xs) -> h2
         | (x1::xs1,x2::xs2) -> if v1 = val_of_card x1 then loop xs1 xs2
-                          else if (val_of_card x1) < (val_of_card x2) then hnd2
-                          else hnd1
+                          else if (val_of_card x1) < (val_of_card x2) then h2
+                          else h1
                         in loop h1 h2
 
-(* Straight comparison *)
-
+(* Straight comparison - returns the better hand
+* Does not work *)
 let rec compare_straight (h1:card list) (h2:card list) : card list =
   match (insertion_sort h1,insertion_sort h2) with
-  | ([],[]) -> failwith "same hands"
+  | ([],[]) -> let r = Random.int 2 in
+               if r = 0 then h1 else h2
   (* because a straight with an A will be AKQJT or A5432 *)
-  | (x1::xs1,x2::xs2) -> if val_of_card x1 = val_of_card x2
-                         then compare_straight xs1 xs2
-                         else compare_high_card [x1] [x2]
-  | _ -> failwith "not possible"
+  | (h11::h12::h13::t1,h21::h22::h23::t2) ->
+    let highCard1 =
+    if one_step_below h12 h11 && one_step_below h13 h12 then h11
+    else if one_step_below h13 h12 then h12 else h13 in
+    let highCard2 =
+    if one_step_below h22 h21 && one_step_below h23 h22 then h21
+    else if one_step_below h23 h22 then h22 else h23 in
+    if compare_high_card [highCard1] [highCard2] = [highCard1] then h1 else h2
+  | _ -> failwith "incorrect input to function"
 
-(* Flush comparison *)
-
-let rec compare_flush (h1:card list) (h2:card list) : card list =
-  match (insertion_sort h1,insertion_sort h2) with
-  | ([],[]) -> failwith "same hands"
-  | (x1::xs1,x2::xs2) -> if val_of_card x1 = val_of_card x2
-                         then compare_flush xs1 xs2
-                         else compare_high_card [x1] [x2]
-  | _ -> failwith "not possible"
+(* Flush comparison - returns the better hand *)
+let compare_flush (h1:card list) (h2:card list) : card list =
+  let s1 = most_common_suit h1 in
+  let s2 = most_common_suit h2 in
+  let newh1 = (insertion_sort_suit (insertion_sort h1)) in
+  let newh2 = (insertion_sort_suit (insertion_sort h2)) in
+  let rec loop hnd1 hnd2 =
+    match (hnd1,hnd2) with
+    | ([],[]) -> let r = Random.int 2 in
+                 if r = 0 then h1 else h2
+    | (x::xs,[]) -> h1
+    | ([],x::xs) -> h2
+    | (x1::xs1,x2::xs2) -> if val_of_card x1 <> val_of_card x2 &&
+                           suit_of_card x1 = fst s1 && suit_of_card x2 = fst s2
+                           then (if compare_high_card [x1] [x2] = [x1] then h1
+                           else h2)
+                           else loop xs1 xs2
+                         in loop newh1 newh2
 
 (* FullHouse comparison *)
-
 let rec compare_full_house (h1:card list) (h2:card list) : card list =
   let tripleValue1 = value_of_triple h1 in
   let tripleValue2 = value_of_triple h2 in
   if tripleValue1 > tripleValue2 then h1
   else if tripleValue1 < tripleValue2 then h2
   else let rec loop hnd1 hnd2 =
-  match (h1,h2) with
-  | ([],[]) -> failwith "same hands"
+  match (insertion_sort hnd1,insertion_sort hnd2) with
+  | ([],[]) -> let r = Random.int 2 in
+               if r = 0 then h1 else h2
+  | (x::xs,[]) -> h1
+  | ([],x::xs) -> h2
   | (x1::xs1,x2::xs2) -> if val_of_card x1 = tripleValue1
                          then loop xs1 xs2
                          else
                          (if val_of_card x1 > val_of_card x2 then h1
                          else if val_of_card x1 < val_of_card x2 then h2
                          else h1) (*same hands*)
-  | _ -> failwith "not possible"
   in loop h1 h2
 
-(* Quads comparison *)
-
+(* Quads comparison - returns the better hand*)
 let rec compare_quads (h1:card list) (h2:card list) : card list =
   match (insertion_sort h1,insertion_sort h2) with
-  | ([],[]) -> failwith "same hands"
-  (* because a straight with an A will be AKQJT or A5432 *)
+  | ([],[]) -> let r = Random.int 2 in
+               if r = 0 then h1 else h2
+  | (x::xs,[]) -> h1
+  | ([],x::xs) -> h2
   | (fst1::snd1::t1,fst2::snd2::t2) ->
     if val_of_card fst1 = val_of_card snd1 &&
     val_of_card fst2 = val_of_card snd2
     then (if val_of_card fst1 = val_of_card fst2 then compare_high_card h1 h2
-    else if val_of_card fst1 > val_of_card fst2 then h1
-    else h2)
+    else if val_of_card fst1 > val_of_card fst2 then h1 else h2)
     else compare_quads (snd1::t1) (snd2::t2)
-  | _ -> failwith "not possible"
+  | _ -> failwith "incorrect input to function"
 
 (* StraightFlush comparison *)
-
-(*same as comparing a regular straight*)
-let rec compare_straight_flush (h1:card list) (h2:card list) : card list =
+(*same as comparing a regular straight - which is not working yet*)
+let compare_straight_flush (h1:card list) (h2:card list) : card list =
   compare_straight h1 h2
 
 (* RoyalFlush comparison *)
-
 (* The only time both players can have a royal flush is when the
-* 5 board cards make a royal flush so this will split the pot *)
-let rec compare_royal_flush (h1:card list) (h2:card list) : card list =
-  failwith "Same hands"
+* 5 board cards make a royal flush so this is automatically a tie *)
+let compare_royal_flush (h1:card list) (h2:card list) : card list =
+  | ([],[]) -> let r = Random.int 2 in
+               if r = 0 then h1 else h2
+
+(*above here works - except for exceptions*)
+
 (* A function for converting cards to a player's BEST POSSIBLE hand.
 * It will presumably be used extensively in the game to determine the winner
 * of the hand.  Gamestate will call this function on each player's hands
@@ -469,9 +501,9 @@ let hand_to_string_helper (clist:card list) (s:string) : string =
   let rec loop cards acc =
     match (insertion_sort cards) with
     | [] -> acc
-    | h::t -> if acc = "" then loop t acc^card_to_string h
-              else loop t acc^" and "^card_to_string h
-  in s1^loop clist ""
+    | h::t -> if acc = "" then loop t (acc^card_to_string h)
+              else loop t (acc^" and "^card_to_string h)
+  in s1^(loop clist ""
 
 let hand_to_string (h:hand) : string =
 match h with
